f6b07d0e23fedc6147cc3831831cd1b448ee8353
nodeminderjs <nodeminderjs@gmail.com>
2013-01-03 18:08:57 -0200

    renaming some files

--- app/app.js
+++ app/app.js
@@ -2,8 +2,8 @@
 //
 var server = require("./server");
 var router = require("./router");
-//var grab   = require("./grab");
-var grab   = require("./grabcc");
+//var grab   = require("./grabm"); // use motion to grab frames
+var grab   = require("./grabc");   // use c code to grab frames
 var config = require("./config");
 
 config.loadConfig();
